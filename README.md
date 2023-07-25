1bf8f8f3aef1bc22c5a41dc9653df2875627e5b3
C Ding <cding@cs.rochester.edu>
2023-07-24 21:14:59 -0400

    2 tenancy model

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Install GSL on the local machine, e.g. brew install gsl on MacOS.
 
 Add the lib path, e.g. export LIBRARY_PATH=/opt/homebrew/lib
+
+Example use:
+
+	cargo run -- -l 3 -s 1 -p 0.5
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
+use clap::Parser;
+
+//use rgsl::types::*;
+use rgsl::randist::binomial::binomial_pdf;
+
+#[derive(Parser)]
+//#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    long: f64,
+    #[arg(short, long)]
+    short: f64,
+    #[arg(short, long)]
+    plong: f64,
+}
+
 fn main() {
 
+    let args = Args::parse();
+
+    let pcs = args.long * args.plong + args.short * (1.0 - args.plong);
+
+    let ibase = (args.long * args.plong + 1.0).floor() as u32;
+
+    let ulong = args.long as u32;
     
+    let mut overage = 0.0;
+    for i in ibase .. (ulong +1) {
+	overage += binomial_pdf(i, args.plong, ulong)
+	    * (i as f64 - args.plong*args.long)*(args.long - args.short) / args.long;
+    }
+
+    let unstored = (args.plong * args.long.powi(2) + (1.0 - args.plong) * args.short.powi(2))
+	/ 2.0 / pcs;
     
     // binomial_P(k: u32, p: f64, n: u32) -> f64
 	
-    println!("Hello, world!");
+    println!("{}", overage/unstored);
 }
